b7118b4b5e9bc110e01f50ddf6480587418a39a4
Hafiz Khairudin <ibnukamy@gmail.com>
2015-09-07 22:20:38 +0800

    Initial Commit

---
+++ FizzBuzz/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  FizzBuzz
+//
+//  Created by Hafiz Khairudin on 9/7/15.
+//  Copyright © 2015 Hafiz Khairudin. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
